978068f0315cfdf1ab7211574ee6eea370c18f12
David Wu <david10608@gmail.com>
2018-02-09 23:30:45 +0100

    InputButton styling

--- App/Components/Styles/InputButtonStyle.js
+++ App/Components/Styles/InputButtonStyle.js
@@ -1,7 +1,17 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-  container: {
-    flex: 1
+  inputButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.5,
+    borderColor: '#91AA9D'
+  },
+
+  inputButtonText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'white'
   }
 })
