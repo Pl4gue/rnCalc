3f99641b3744d658820b158a3691184041a314a5
David Wu <david10608@gmail.com>
2018-02-09 23:23:36 +0100

    Transition Config

---
+++ App/Navigation/TransistionConfiguration.js
@@ -0,0 +1,32 @@
+export default let TransitionConfiguration = () => {
+    return {
+        // Define scene interpolation, eq. custom transition
+        screenInterpolator: (sceneProps) => {
+            const {position, scene} = sceneProps;
+            const {index} = scene;
+
+            return MyTransition(index, position);
+        }
+    }
+};
+
+let MyTransition = (index, position) => {
+    const inputRange = [index - 1, index, index + 1];
+    const outputRange = [.8, 1, 1];
+    const opacity = position.interpolate({
+        inputRange,
+        outputRange,
+    });
+
+    const scaleY = position.interpolate({
+        inputRange,
+        outputRange,
+    });
+
+    return {
+    opacity,
+        transform: [
+            {scaleY}
+        ]
+    };
+};
