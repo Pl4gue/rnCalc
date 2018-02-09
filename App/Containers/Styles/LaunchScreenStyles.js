d2586a63d2f354ea84db70879f18870d448da1c0
David Wu <david10608@gmail.com>
2018-02-09 23:22:55 +0100

    LaunchScreen button and container styling

--- App/Containers/Styles/LaunchScreenStyles.js
+++ App/Containers/Styles/LaunchScreenStyles.js
@@ -4,6 +4,7 @@ import { Metrics, ApplicationStyles } from '../../Themes/'
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   container: {
+    paddingTop: Metrics.baseMargin,
     paddingBottom: Metrics.baseMargin
   },
   logo: {
@@ -14,5 +15,39 @@ export default StyleSheet.create({
   },
   centered: {
     alignItems: 'center'
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginTop: Metrics.doubleSection
+  },
+  calcButton: {
+    backgroundColor: "#3E606F",
+    width: 250,
+    height: 5 * Metrics.doubleBaseMargin,
+    borderColor: "transparent",
+    borderWidth: 5,
+    borderRadius: 15
+  },
+  calcButton_font: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: "700"
+  },
+  text: {
+    flex: 1,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#ffffff',
+    marginTop: Metrics.doubleBaseMargin
+  },
+  authorContainer: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#ffffff',
+  },
+  author: {
+    textDecorationLine: 'underline'
   }
 })
