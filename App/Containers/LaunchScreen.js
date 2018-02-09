78b4b733c491395783c21b771741b02386541dbf
David Wu <david10608@gmail.com>
2018-02-09 23:22:10 +0100

    LaunchScreen - Open Calculator

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
+import { ScrollView, Image, View, Linking} from 'react-native'
 import { Images } from '../Themes'
+
+import { Button, Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
@@ -9,18 +12,29 @@ export default class LaunchScreen extends Component {
   render () {
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
+
           <View style={styles.centered}>
             <Image source={Images.launch} style={styles.logo} />
           </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
+          <Text style={styles.text}>
+              {"React Native Calculator"}
+          </Text>
+          <Text style={styles.authorContainer}>
+              <Text>by </Text>
+              <Text style={styles.author} 
+                onPress={() => Linking.openURL('http://david-wu.me')}>
+              {"David Wu (Pl4gue)"}
+              </Text>
+          </Text>
+          <View style={styles.buttonContainer}>
+            <Button
+              title={"Open\nCalculator"}
+              textStyle={styles.calcButton_font}
+              buttonStyle={styles.calcButton}
+              onPress={() => this.props.navigation.navigate('CalculatorScreen')}
+            />
+           </View>
 
         </ScrollView>
       </View>
